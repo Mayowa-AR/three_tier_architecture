b8b5eacf39329b5da812051bc37b713637a15bb0
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-05 23:16:44 +0100

    update route table association with private_subnet

--- route_table_association.tf
+++ route_table_association.tf
@@ -7,3 +7,8 @@ resource "aws_route_table_association" "subnet_rt_public_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_route_table_association" "subnet_rt_private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
