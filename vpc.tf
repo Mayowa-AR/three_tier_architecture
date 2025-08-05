fc311059c55f86a93e970e539642820874dcfb47
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-05 17:51:52 +0100

    Added Terraform configuration files

---
+++ vpc.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+ region = var.region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  instance_tenancy     = "default"
+  tags = {
+    NAME = "Apache-VPC-Updated"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1a"
+
+  tags = {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1b"
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+}
+
+
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1c"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
