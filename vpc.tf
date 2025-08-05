9aa54e5dc1563db1d3397f0a22f02ab7876e5299
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-05 22:24:34 +0100

    updated instance tenancy

--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 resource "aws_vpc" "three_tier_vpc" {
   cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   instance_tenancy     = "default"
   tags = {
-    NAME = "Apache_vpc_three_tier"
+    Name = "Apache_vpc_three_tier"
   }
 }
 
