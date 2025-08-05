5fa9357d7466f62e95fcc85c029109b3f5e7580a
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-05 22:17:24 +0100

    Updated eips tag name

--- elastic_ip.tf
+++ elastic_ip.tf
@@ -1,9 +1,17 @@
 resource "aws_eip" "nat1" {
   #Use depends_on to set an explicit dependency on internet gateway.
   depends_on = [aws_internet_gateway.main_igw]
+
+  tags = {
+    Name = "NAT-GW-EIP-1"
+  }
 }
 
 resource "aws_eip" "nat2" {
   #Use depends_on to set an explicit dependency on internet gateway.
   depends_on = [aws_internet_gateway.main_igw]
+
+  tags = {
+    Name = "NAT-GW-EIP-2"
+  }
 }
