ee56f0829fcb53c00929e688c34a57921d19da7f
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-05 18:45:40 +0100

    Update IGW

--- vpc.tf
+++ vpc.tf
@@ -2,18 +2,18 @@ provider "aws" {
  region = var.region
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "three_tier_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   instance_tenancy     = "default"
   tags = {
-    NAME = "Apache-VPC-Updated"
+    NAME = "Apache_vpc_three_tier"
   }
 }
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.three_tier_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1a"
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_subnet_1" {
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.three_tier_vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1b"
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_subnet_2" {
 
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.three_tier_vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-1c"
@@ -46,4 +46,16 @@ resource "aws_subnet" "private_subnet" {
     Name = "PrivateSubnet"
   }
 }
+
+
+resource "aws_internet_gateway" "cba_igw" {
+  vpc_id = aws_vpc.three_tier_vpc.id
+
+  tags = {
+    Name = "ApacheIGW"
+  }
+}
+
+
+
 
