397bf0b0db9eea569693308ae81605d3dec25ec0
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-06 11:40:25 +0100

    update instance with private ec2 instance block

--- instance.tf
+++ instance.tf
@@ -32,6 +32,24 @@ resource "aws_instance" "web_2" {
 
 }
 
+
+# Ec2 instance in private subnet
+resource "aws_instance" "private_ec2" {
+  ami             = data.aws_ssm_parameter.instance_ami.value
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.private_subnet.id
+  security_groups = [aws_security_group.private_sg.id]
+  key_name        = var.key_name
+  user_data       = fileexists("install_apache.sh") ? file("install_apache.sh") : null
+
+
+  tags = {
+    "Name" = "Web-Server-1"
+  }
+
+  
+}
+
 data "aws_ssm_parameter" "instance_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
